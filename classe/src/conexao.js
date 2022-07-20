3bbc0f464584efe986220d36bdff2d3a5f68d863
Usuario <Usurio@DESKTOP-KLN9T82>
2022-07-20 18:21:01 -0300

    config conexao com banco de dados na hek=roku

--- classe/src/conexao.js
+++ classe/src/conexao.js
@@ -2,12 +2,13 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        port: 1302,
-        user: 'postgres',
-        password: '1302',
-        database: 'mercadinho'
+        host: 'ec2-34-235-198-25.compute-1.amazonaws.com',
+        port: 5432,
+        user: 'ufasjflihvqjey',
+        password: 'db2f8b75b22b5088cd3019eac411b5b39b36b5eec8465a8dea6e143501e9152e',
+        database: 'd27odh2entdf86'
     }
 });
 
 module.exports = knex;
+
